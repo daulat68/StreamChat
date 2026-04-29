802648aa46f9746081c4120bcbe5a4040b5837ab
daulat <daulatsingh7878@gmail.com>
2026-04-29 15:45:42 +0530

    change in naming

--- Frontend/src/components/NoChatSelected.jsx
+++ Frontend/src/components/NoChatSelected.jsx
@@ -16,7 +16,7 @@ const NoChatSelected = () => {
             </div>
             </div>
 
-            <h2 className="text-2xl font-bold">Welcome to Chat App</h2>
+            <h2 className="text-2xl font-bold">Welcome to StreamChat</h2>
             <p className="text-base-content/60">
             Select a conversation from the sidebar to start chatting
             </p>
